b257178d66182efdf6518d317dda71d4a5757e31
liamhardy742 <122562952+liamhardy742@users.noreply.github.com>
2024-02-16 21:29:56 -0800

    Added a wide range of comments. Removed unnecessary code. This user story ended up being implemented in the implementation of US4

--- app/src/main/java/edu/ucsd/cse110/successorator/data/db/RoomMostImportantThingRepository.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/data/db/RoomMostImportantThingRepository.java
@@ -19,7 +19,7 @@ public class RoomMostImportantThingRepository implements MostImportantThingRepos
         this.mostImportantThingDao = mostImportantThingDao;
     }
 
-//    @Override
+    @Override
     public Subject<MostImportantThing> find(int id) {
         LiveData<MostImportantThingEntity> entityLiveData = mostImportantThingDao.findAsLiveData(id);
         // this map takes a LiveData<X> and turns it into a LiveData<Y>
@@ -27,7 +27,7 @@ public class RoomMostImportantThingRepository implements MostImportantThingRepos
         return new LiveDataSubjectAdapter<>(mostImportantThingLiveData);
     }
 
-//    @Override
+    @Override
     public Subject<List<MostImportantThing>> findAll() {
         var entitiesLiveData = mostImportantThingDao.findAllAsLiveData();
         var mostImportantThingsLiveData = Transformations.map(entitiesLiveData, entities -> {
@@ -38,12 +38,12 @@ public class RoomMostImportantThingRepository implements MostImportantThingRepos
         return new LiveDataSubjectAdapter<>(mostImportantThingsLiveData);
     }
 
-//    @Override
+    @Override
     public void save(MostImportantThing mostImportantThing) {
         this.mostImportantThingDao.insert(MostImportantThingEntity.fromMostImportantThing(mostImportantThing));
     }
 
-//    @Override
+    @Override
     public void save(List<MostImportantThing> mostImportantThings) {
         var entities = mostImportantThings.stream()
                 .map(MostImportantThingEntity::fromMostImportantThing)
@@ -51,17 +51,17 @@ public class RoomMostImportantThingRepository implements MostImportantThingRepos
         this.mostImportantThingDao.insert(entities);
     }
 
-//    @Override
+    @Override
     public void prepend(MostImportantThing mostImportantThing) {
         mostImportantThingDao.prepend(MostImportantThingEntity.fromMostImportantThing(mostImportantThing));
     }
 
-//    @Override
+    @Override
     public void append(MostImportantThing mostImportantThing) {
         mostImportantThingDao.append(MostImportantThingEntity.fromMostImportantThing(mostImportantThing));
     }
 
-//    @Override
+    @Override
     public void remove(int id) {
         mostImportantThingDao.delete(id);
     }
@@ -73,66 +73,85 @@ public class RoomMostImportantThingRepository implements MostImportantThingRepos
     public void toggleCompleted(int id) {
         System.out.println("Toggling completed");
         if (this.mostImportantThingDao.find(id).completed) {
+            //Move the item to the aboslute top of the list
             this.moveToTop(id);
         }
         else {
+            //If te item was not done, move it to the top of the finished
+            //portion of the list (This is US8 that was already implemented
+            //During the implementation of US4)
             this.moveToTopOfFinished(id);
         }
         this.mostImportantThingDao.toggleCompleted(id);
     }
 
+    //Moves an MIT to the abolsute top of the list
     public void moveToTop(int id) {
         this.mostImportantThingDao.prepend(this.mostImportantThingDao.find(id));
     }
 
+    //Moves an MIT to the top of the unfinished portion of the list
+    //This means below all unfinished tasks, but above all finished tasks
     public void moveToTopOfFinished(int id) {
         var ElementList = this.mostImportantThingDao.findAll();
         int numElems = ElementList.size();
         int insertIdx = 0;
+        //Find the Index of the first element where the next element is completed
         for (int i = 0; i < numElems; i++) {
             if (ElementList.get(i).completed && (ElementList.get(i).id != id)) {
                 break;
             }
             insertIdx++;
         }
-        System.out.println("InsertIdx is " + insertIdx);
+        //If there are no unfinished tasks that aren't the one we're moving
         if (insertIdx == 0) {
-            this.mostImportantThingDao.shiftSortOrders(this.mostImportantThingDao.getMinSortOrder(), this.mostImportantThingDao.getMaxSortOrder(), 1);
-
+            /* We do not need to need to do any reorganizing, as this unfinished
+            task must already be at the top of the list, which is the desired
+            position */
         }
+        //If there are only unfinished tasks
         else if (insertIdx == numElems) {
             this.mostImportantThingDao.append(this.mostImportantThingDao.find(id));
         }
+        //There are both finished and unfinished tasks
         else {
             int sortOrder = ElementList.get(insertIdx).toMostImportantThing().sortOrder();
             this.mostImportantThingDao.shiftSortOrders(ElementList.get(insertIdx).toMostImportantThing().sortOrder(), this.mostImportantThingDao.getMaxSortOrder(), 1);
             this.mostImportantThingDao.insert(MostImportantThingEntity.fromMostImportantThing(this.mostImportantThingDao.find(id).toMostImportantThing().withSortOrder(sortOrder)));
-
         }
     }
 
+    //Adds a new MIT in the correct order, below all of the unfinished MITs
+    //And above all of the finished MITs
     public void addNewMostImportantThing(MostImportantThing mit) {
         var ElementList = this.mostImportantThingDao.findAll();
         int numElems = ElementList.size();
         int insertIdx = 0;
+        //Find index of the first element where the next element is completed
         for (int i = 0; i < numElems; i++) {
             if (ElementList.get(i).completed) {
                 break;
             }
             insertIdx++;
         }
-        System.out.println("InsertIdx is " + insertIdx);
+        //If there are no elements in the list
         if (numElems == 0) {
             this.mostImportantThingDao.append(MostImportantThingEntity.fromMostImportantThing(mit));
         }
+        //If there are only completed MITs in the list
         else if (insertIdx == 0) {
+            //Add to the front of the list
             this.mostImportantThingDao.shiftSortOrders(this.mostImportantThingDao.getMinSortOrder(), this.mostImportantThingDao.getMaxSortOrder(), 1);
             this.mostImportantThingDao.insert(MostImportantThingEntity.fromMostImportantThing(mit.withSortOrder(this.mostImportantThingDao.getMinSortOrder() - 1)));
         }
+        //If there are only uncompleted MITs in the list
         else if (insertIdx == numElems) {
+            //Add to the bottom of the list
             this.mostImportantThingDao.append(MostImportantThingEntity.fromMostImportantThing(mit));
         }
+        //There are uncompleted and completed MITs
         else {
+            //Shift all completed MITs down, insert new one before them
             int sortOrder = ElementList.get(insertIdx).toMostImportantThing().sortOrder();
             this.mostImportantThingDao.shiftSortOrders(ElementList.get(insertIdx).toMostImportantThing().sortOrder(), this.mostImportantThingDao.getMaxSortOrder(), 1);
             this.mostImportantThingDao.insert(MostImportantThingEntity.fromMostImportantThing(mit.withSortOrder(sortOrder)));
