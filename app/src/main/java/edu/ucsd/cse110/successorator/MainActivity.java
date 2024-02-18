355c0b75a27088f7708117b69f56f0a1fc913f72
unknown-ap <annie.ping09@gmail.com>
2024-02-17 17:09:16 -0800

    created button to advance date

--- app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
@@ -3,6 +3,7 @@ package edu.ucsd.cse110.successorator;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextClock;
@@ -15,12 +16,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 import edu.ucsd.cse110.successorator.databinding.ActivityMainBinding;
 import edu.ucsd.cse110.successorator.ui.dialog.CreateMitDialogFragment;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding view;
+    private TextView dateTextView;
+    private int incrementDateBy = 0;
 
 
     private MainViewModel activityModel;
@@ -30,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         setTitle(R.string.app_title);
 
         this.view = ActivityMainBinding.inflate(getLayoutInflater());//, null, false);
+        dateTextView = findViewById(R.id.action_bar_menu_advance_date);
+
         setContentView(view.getRoot());
 
     }
@@ -46,20 +52,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        incrementDateBy++;
         var itemId = item.getItemId();
 
+        if (itemId == R.id.action_bar_menu_advance_date) {
+            advanceDate(incrementDateBy);
+        }
         if (itemId == R.id.action_bar_menu_add_mit) {
             //Todo, make button initiate a Dialog
             var dialogFragment = CreateMitDialogFragment.newInstance();
             dialogFragment.show(getSupportFragmentManager(), "CreateMitDialogFragment");
-
         }
-
-//        if (itemId == R.id.action_bar_menu_date) {
-//
-//        }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    //only advances the date by 1 day
+    //restarting the app will reset to current day
+    public void advanceDate(int incrementDateBy) {
+        dateTextView = findViewById(R.id.action_bar_menu_date);
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE MM/dd");
+        c.add(Calendar.DAY_OF_YEAR, incrementDateBy);
+        String date = dateFormat.format(c.getTime());
+        dateTextView.setText(date);
     }
 
 
