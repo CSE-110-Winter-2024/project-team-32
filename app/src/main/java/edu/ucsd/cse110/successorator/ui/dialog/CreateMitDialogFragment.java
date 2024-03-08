0f1cec290fed5402356594d0d0e131ee19de5d71
liamhardy742 <122562952+liamhardy742@users.noreply.github.com>
2024-03-08 00:42:47 -0800

    Added date formatting to properly display 'weekly on XX' options - Task 1

--- app/src/main/java/edu/ucsd/cse110/successorator/ui/dialog/CreateMitDialogFragment.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/ui/dialog/CreateMitDialogFragment.java
@@ -1,5 +1,8 @@
 package edu.ucsd.cse110.successorator.ui.dialog;
 
+import static edu.ucsd.cse110.successorator.ui.RecurringMitListAdapter.convToDayOfWeek;
+import static edu.ucsd.cse110.successorator.ui.RecurringMitListAdapter.convToNthOccurenceInMonth;
+
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
@@ -10,7 +13,10 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import edu.ucsd.cse110.successorator.MainViewModel;
 import edu.ucsd.cse110.successorator.R;
@@ -67,6 +73,17 @@ public class CreateMitDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         this.view = FragmentDialogCreateMitBinding.inflate(getLayoutInflater());
+        Date currTime = new Date(); // Default to current time for TODAY_VIEW
+        if (currentView == 1) { // Corresponds to Tomorrow view
+            currTime = new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(1));
+        }
+        String weeklyText = "Weekly " + formatReccurencePeriod("Weekly", currTime);
+        this.view.weeklyRadioButton.setText(weeklyText);
+        String monthlyText = "Monthly " + formatReccurencePeriod("Monthly", currTime);
+        this.view.monthlyRadioButton.setText(monthlyText);
+        String yearlyText = "Yearly " + formatReccurencePeriod("Yearly", currTime);
+        this.view.yearlyRadioButton.setText(yearlyText);
+
         return new AlertDialog.Builder(getActivity())
                 .setTitle("Enter your new task!")
                 .setView(view.getRoot())
@@ -124,6 +141,36 @@ public class CreateMitDialogFragment extends DialogFragment {
      */
     public void onNegativeButtonClick(DialogInterface dialog, int which) {
         dialog.cancel();
+    }
+
+    private String formatReccurencePeriod(String recurPeriod, Date date) {
+        String recurText = "";
+        if (recurPeriod.equals("Weekly")) {
+            //Calculate the day of week the task was made on
+            String dayOfWeekString = convToDayOfWeek(date);
+            //Use DayOfWeek to get the string for day instead of integer
+            recurText = recurText + " on " + dayOfWeekString;
+        }
+        else if (recurPeriod.equals("Monthly")) {
+            String dayOfWeekString = convToDayOfWeek(date);
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(date);
+            int dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
+            //Calculate which occurrence of the day it is in the month
+            //e.g. 1st Tuesday, 2nd Tuesday, 3rd Tuesday
+            int occurrenceOfDay = (dayOfMonth / 7);
+            //Convert from 0, 1, 2, to 1st, 2nd, 3rd
+            String occurrenceOfDayString = convToNthOccurenceInMonth(occurrenceOfDay);
+
+            recurText = recurText + " " + occurrenceOfDayString + " " + dayOfWeekString;
+
+        }
+        else if (recurPeriod.equals("Yearly")) {
+            SimpleDateFormat format = new SimpleDateFormat("MM/dd");
+            String dateInYear = format.format(date);
+            recurText = recurText + " on " + dateInYear;
+        }
+        return recurText;
     }
 
 }
