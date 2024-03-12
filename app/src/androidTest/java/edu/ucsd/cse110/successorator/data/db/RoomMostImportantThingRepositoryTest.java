6f9658468d633826e6b3dad51448414158c835a1
liamhardy742 <122562952+liamhardy742@users.noreply.github.com>
2024-03-12 11:56:18 -0700

    Fixed mock tester - Task N/A

--- app/src/androidTest/java/edu/ucsd/cse110/successorator/data/db/RoomMostImportantThingRepositoryTest.java
+++ app/src/androidTest/java/edu/ucsd/cse110/successorator/data/db/RoomMostImportantThingRepositoryTest.java
@@ -167,7 +167,17 @@ public class RoomMostImportantThingRepositoryTest {
         }
 
         @Override
+        public LiveData<List<MostImportantThingEntity>> findAllPendingAsLiveData(String context) {
+            return null;
+        }
+
+        @Override
         public LiveData<List<MostImportantThingEntity>> findAllRecurringAsLiveData() {
+            return null;
+        }
+
+        @Override
+        public LiveData<List<MostImportantThingEntity>> findAllRecurringAsLiveData(String context) {
             return null;
         }
 
